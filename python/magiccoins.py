2e68974aaa9f94328a17ed0f130e4835c750605e
lenulek <lenulek@gmail.com>
2019-12-01 16:11:46 +0300

    :star: update: shape

--- python/magiccoins.py
+++ python/magiccoins.py
@@ -20,6 +20,7 @@ print('''Эх... не вдвое, ну и что ж?!
 Этот результат тоже хорош!
 Так за раз монеток %s
 Можно много их наделать!''' % magic_coins)
+print()
 days_in_week = 7
 magcoins_inweek = magic_coins * days_in_week
 print('''Дней в неделе целых %s
@@ -35,27 +36,31 @@ stolen_coins = 3
 print('''Ай-я-яй! Ты посмотри:
 Эта гадкая ворона
 Прилетела из-за забора
-И утащила монетки %s''' % stolen_coins)
+И утащила монетки %s.''' % stolen_coins)
 print()
 print('''Раз в неделю так таскать,
 Сколько ж можно потерять?
 Посчитаю наперёд,
 Сколько минусов за год.''')
 print()
+print('/считает/')
+print()
 coins = found_coins
 for week in range (1, 53):
     coins = coins + magcoins_inweek - stolen_coins
-    print('Неделя %s = %s денежек' % (week, coins))
+    print('Неделя %s = %s денежек будет у меня' % (week, coins))
 print()
 print('''Если б чучело поставить,
 Чтоб ворону напугать.
 Заработал бы я больше.
 Сколько? - Надо посчитать!''')
 print()
+print('/считает/')
+print()
 coins = found_coins
 for week in range (1, 53):
     coins = coins + magcoins_inweek
-    print('Неделя %s = %s денежек' % (week, coins))
+    print('Неделя %s = %s денежек будет у меня' % (week, coins))
 
 print()
 print('''Вот так был бы я богат,
